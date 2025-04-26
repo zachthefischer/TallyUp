90335ebdc6fbb586fd42bfcb61a09c12f2666b01
Zach Fischer <zfischer42@gmail.com>
2025-04-25 22:41:02 -0700

    im losing my mind

--- web-app/src/pages/Contact.tsx
+++
@@ -1,8 +0,0 @@
-// src/components/Contact.js
-import React from 'react';
-
-const Contact = () => {
-  return <h2>Contact Page</h2>;
-};
-
-export default Contact;
--- web-app/src/pages/NoMatch.tsx
+++
@@ -1,8 +0,0 @@
-// src/components/Home.js
-import React from 'react';
-
-const Home = () => {
-  return <h2>Home Page</h2>;
-};
-
-export default Home;
