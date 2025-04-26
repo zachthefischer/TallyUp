159c039e7fb762b4a67cd1c8f2c5fa5c337bdb88
E S <shivakumar.esha12@gmail.com>
2025-04-26 01:00:46 -0700

    with new ui

--- web-app/src/App.tsx
+++ web-app/src/App.tsx
@@ -3,6 +3,8 @@ import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import AdminPage from './pages/AdminPage';
 import Dashboard from './pages/Dashboard';
+import ClubFinanceManager from './components/ClubFinanceManager';
+
 import './App.css'
 import NavBar from './components/Navbar';
 import SignupPage from './pages/SignupPage';
@@ -17,15 +19,14 @@ function App() {
           {/* Public Routes: No Navbar */}
           <Route path="/signup" element={<SignupPage />} />
           <Route path="/login" element={<LoginPage />} />
+          <Route path="/dashboard" element={<Dashboard />} />
 
           {/* Protected Routes: With Navbar */}
           <Route path="/" element={
             <>
               <div className="min-h-screen bg-gray-100">
                 <NavBar />
-                <div className="container mx-auto px-4 py-6">
-                  <h1 className="text-center text-4xl">Home Page</h1>
-                </div>
+                <ClubFinanceManager />
               </div>
             </>
           } />
